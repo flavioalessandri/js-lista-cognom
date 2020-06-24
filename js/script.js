d5befb64780c29f07370c03a110413cf317684af
Flavio <flavio.alessandri@gmail.com>
2020-06-24 16:24:49 +0200

    JS - List - work in progress

--- js/script.js
+++ js/script.js
@@ -4,22 +4,23 @@
 //insert a btn var for the button
 var btn= document.getElementById("btn");
 
+var btnCear= document.getElementById("btnClear");
+
 //After  user click the button
 btn.addEventListener("click", function() {
   var list =["Neroni","Bianchi", "Verdi", "Rossi", "Gialli"];
   console.log(list);
   var familyName = document.getElementById("familyName").value;
 
-  var count= false;
-  var tag =0;
+  list.push(familyName); //add user familyName to our list
 
-  while (!isNaN(familyName) ){  //verify User data
-    alert("Please insert your real FamilyName");
-    break;
-    }
-    console.log("Cognome inserito", familyName);
-    list.push(familyName); //add user familyName to our list
-    console.log(list);
+  for (var i =0; i<list.length;i++){
+    var lista = list[i];
+    // console.log(lista);
+    var orderedList= list.sort();
+    var familyNameIndex = orderedList.indexOf(familyName);
+    document.getElementById("position").innerHTML = "Il tuo cognome è alla posizione numero " + (familyNameIndex+1) + " della lista!" ;
+    document.getElementById("familyNameList").innerHTML += "<li>"+lista+"</li>";
+}
 
-
-});
+});//addEventListener End
