2ce2509e0873508e3d8abbd3e1ed340ba0ef337e
Flavio <flavio.alessandri@gmail.com>
2020-06-24 16:45:48 +0200

    JS -add clear button

--- js/script.js
+++ js/script.js
@@ -9,18 +9,27 @@ var btnCear= document.getElementById("btnClear");
 //After  user click the button
 btn.addEventListener("click", function() {
   var list =["Neroni","Bianchi", "Verdi", "Rossi", "Gialli"];
-  console.log(list);
   var familyName = document.getElementById("familyName").value;
 
   list.push(familyName); //add user familyName to our list
+  console.log(list);
+  list.sort();
+  console.log(list);
 
-  for (var i =0; i<list.length;i++){
+  for (var i =0; i< list.length;i++){
     var lista = list[i];
-    // console.log(lista);
-    var orderedList= list.sort();
-    var familyNameIndex = orderedList.indexOf(familyName);
+    console.log(lista);
+    var familyNameIndex = list.indexOf(familyName);
     document.getElementById("position").innerHTML = "Il tuo cognome è alla posizione numero " + (familyNameIndex+1) + " della lista!" ;
     document.getElementById("familyNameList").innerHTML += "<li>"+lista+"</li>";
 }
+
+});//addEventListener End
+
+var btnClear = document.getElementById("btnClear");
+btnClear.addEventListener("click", function() {
+
+document.getElementById("familyNameList").innerHTML = "";
+document.getElementById("position").innerHTML = "";
 
 });//addEventListener End
