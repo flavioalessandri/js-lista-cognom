e775afb09bb4b501d2291627c7fb2382f0b5550b
Flavio <flavio.alessandri@gmail.com>
2020-06-24 18:32:50 +0200

    Js - completed (Css Work in progress)

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 
 //Ask User to fill the familyname field
 
-//insert a btn var for the button
+//insert a btn var to fill the list after click
 var btn= document.getElementById("btn");
-
+//insert a btn var to clear the list after click
 var btnCear= document.getElementById("btnClear");
 
 //After  user click the button
 btn.addEventListener("click", function() {
   var list =["Neroni","Bianchi", "Verdi", "Rossi", "Gialli"];
   var familyName = document.getElementById("familyName").value;
+
+  var tiny = "a";
+
+//verify if User Data are correct: not undefined, not notanumber, first letter is Uppercase
+  if (familyName != undefined && isNaN(familyName) && familyName<tiny){
 
   list.push(familyName); //add user familyName to our list
   console.log(list);
@@ -22,6 +27,10 @@ btn.addEventListener("click", function() {
     var familyNameIndex = list.indexOf(familyName);
     document.getElementById("position").innerHTML = "Il tuo cognome è alla posizione numero " + (familyNameIndex+1) + " della lista!" ;
     document.getElementById("familyNameList").innerHTML += "<li>"+lista+"</li>";
+  }
+
+}else{
+  alert("hai digitato dato errato!");
 }
 
 });//addEventListener End
@@ -31,5 +40,6 @@ btnClear.addEventListener("click", function() {
 
 document.getElementById("familyNameList").innerHTML = "";
 document.getElementById("position").innerHTML = "";
+
 
 });//addEventListener End
